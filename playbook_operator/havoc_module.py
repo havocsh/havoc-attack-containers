7cb5cbf5710a721f7e1936d7005beb2e929b93c6
Tom Daquino <tom@havoc.sh>
2023-05-03 00:52:59 -0700

    Re-inserting short delays to reduce CloudWatch Logs contention.

--- playbook_operator/havoc_module.py
+++ playbook_operator/havoc_module.py
@@ -1040,6 +1040,7 @@ class call_object():
                                     json_value = re.sub(re_sub, str(dep_value), json_value)
                             send_response({'outcome': 'success', 'details': json_value}, 'True', self.user_id,
                                           self.playbook_name, self.playbook_operator_version, f'configure {node_path}', value, self.end_time)
+                            t.sleep(2)
                             value = json.loads(json_value, strict=False)
                             operator_command = f'create {node_path}'
                             method_result = method(object_name, 'create', **value)
@@ -1047,6 +1048,7 @@ class call_object():
                                 send_response({'outcome': 'success', 'details': method_result}, 'True', self.user_id,
                                               self.playbook_name, self.playbook_operator_version, operator_command, value, self.end_time)
                                 executed_list.append(node_path)
+                                t.sleep(2)
                             if 'failed' in method_result:
                                 send_response({'outcome': 'failed', 'details': method_result}, 'True', self.user_id, self.playbook_name,
                                               self.playbook_operator_version, operator_command, value, self.end_time)
@@ -1055,6 +1057,7 @@ class call_object():
                                 if 'action' not in method_result:
                                     return node_path
                                 executed_list.append(node_path)
+                                t.sleep(2)
                             self.exec_order.next_exec_rule(node_path)
                         
     def destroyer(self, playbook_config, executed_list):
@@ -1066,20 +1069,24 @@ class call_object():
                     node_path = f'{section}.{dot_path}'
                     if node_path in executed_list:
                         execution_order, current_rule = self.exec_order.get_exec_order(node_path)
+                        t.sleep(2)
                         if execution_order == current_rule:
                             executed_list.remove(node_path)
                             method, object_name = self.object_resolver(node_path)
                             value = {'destroy_all_resources': True}
                             send_response({'outcome': 'success'}, 'True', self.user_id, self.playbook_name,
                                           self.playbook_operator_version, f'configure {node_path}', value, self.end_time)
+                            t.sleep(2)
                             operator_command = f'delete {node_path}'
                             method_result = method(object_name, 'delete', **value)
                             if 'failed' not in method_result:
                                 send_response({'outcome': 'success', 'details': method_result}, 'True', self.user_id, self.playbook_name,
                                               self.playbook_operator_version, operator_command, value, self.end_time)
+                                t.sleep(2)
                             else:
                                 send_response({'outcome': 'failed', 'details': method_result}, 'True', self.user_id, self.playbook_name,
                                               self.playbook_operator_version, operator_command, value, self.end_time)
+                                t.sleep(2)
                             self.exec_order.prev_exec_rule(node_path)
 
     def execute_playbook(self):
@@ -1178,6 +1185,7 @@ class call_object():
         send_response({'outcome': 'success', 'details': execution_order}, 'True', self.user_id, self.playbook_name, 
                       self.playbook_operator_version, 'create execution order', {'no_args': 'True'}, self.end_time)
         self.exec_order.set_rules(execution_order, node_list)
+        t.sleep(2)
         creator_result = self.creator(playbook_config, node_list, tracking_list)
 
         execution_order = clean_dependencies(get_node_dependencies(DG, tracking_list))
@@ -1186,6 +1194,7 @@ class call_object():
         self.exec_order.set_rules(execution_order, tracking_list)
         if creator_result:
             self.exec_order.exec_rule_failure(tracking_list)
+        t.sleep(2)
         self.destroyer(playbook_config, tracking_list)
 
         return {'outcome': 'success', 'message': 'playbook execution completed', 'forward_log': 'True'}
